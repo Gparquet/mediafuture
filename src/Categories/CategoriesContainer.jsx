767c538fca8ee5d9a32c272f4a76c0ed56cdff62
gparquet <parquet.geoffrey@gmail.com>
2019-12-29 10:07:36 +0100

    refactor: CategoriesContainer with hooks

--- src/Categories/CategoriesContainer.jsx
+++ src/Categories/CategoriesContainer.jsx
@@ -1,57 +1,48 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import Categories from './Categories';
 
-class CategoriesContainer extends Component {
-  url = 'http://localhost:3001/api';
+const url = 'http://localhost:3001/api';
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      albums: [],
-      movies: [],
-      books: [],
-      ebooks: []
-    };
-  }
+const getAlbums = () => {
+  return fetch(`${url}/albums`).then(res => res.json());
+};
+const getMovies = () => {
+  return fetch(`${url}/movies`).then(res => res.json());
+};
 
-  getAlbums() {
-    return fetch(`${this.url}/albums`).then(res => res.json());
-  }
-  getMovies() {
-    return fetch(`${this.url}/movies`).then(res => res.json());
-  }
+const getBooks = () => {
+  return fetch(`${url}/books`).then(res => res.json());
+};
 
-  getBooks() {
-    return fetch(`${this.url}/books`).then(res => res.json());
-  }
+const geteBooks = () => {
+  return fetch(`${url}/ebooks`).then(res => res.json());
+};
 
-  geteBooks() {
-    return fetch(`${this.url}/ebooks`).then(res => res.json());
-  }
+const CategoriesContainer = () => {
+  const [state, setState] = useState({
+    albums: [],
+    movies: [],
+    books: [],
+    ebooks: []
+  });
 
-  componentDidMount() {
-    Promise.all([
-      this.getAlbums(),
-      this.getMovies(),
-      this.geteBooks(),
-      this.geteBooks()
-    ]).then(values => {
-      const albums = values[0].data;
-      const movies = values[1].data;
-      const books = values[2].data;
-      const ebooks = values[3].data;
-      this.setState({
-        albums,
-        movies,
-        books,
-        ebooks
-      });
-    });
-  }
-
-  render() {
-    return <Categories {...this.state} />;
-  }
-}
+  useEffect(() => {
+    Promise.all([getAlbums(), getMovies(), getBooks(), geteBooks()]).then(
+      values => {
+        const albums = values[0].data;
+        const movies = values[1].data;
+        const books = values[2].data;
+        const ebooks = values[3].data;
+        setState({
+          albums,
+          movies,
+          books,
+          ebooks
+        });
+      }
+    );
+  });
+  return <Categories {...state} />;
+};
 
 export default CategoriesContainer;
